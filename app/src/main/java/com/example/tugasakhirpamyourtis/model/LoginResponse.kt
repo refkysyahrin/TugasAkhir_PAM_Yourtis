ea7738d7a1a116b592748d9cb83316d944fb9b61
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-13 10:29:20 +0700

    implementasi data class User dengan detail profil lengkap dan integrasi ke dalam LoginResponse

--- app/src/main/java/com/example/tugasakhirpamyourtis/model/LoginResponse.kt
+++ app/src/main/java/com/example/tugasakhirpamyourtis/model/LoginResponse.kt
@@ -4,3 +4,12 @@ data class LoginResponse (
     val message: String,
     val data: User?
 )
+
+data class User(
+    val id_user: Int,
+    val username: String,
+    val email: String,
+    val role: String,
+    val no_hp: String,
+    val alamat: String
+)
