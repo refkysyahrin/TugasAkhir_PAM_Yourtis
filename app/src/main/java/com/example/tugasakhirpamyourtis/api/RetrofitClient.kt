f90935bbda01763a5da8c5d219aedd225bf93f0a
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-13 10:23:58 +0700

    implementasi RetrofitClient singleton untuk komunikasi API dengan konfigurasi Base URL, Gson converter, dan logging interceptor

---
+++ app/src/main/java/com/example/tugasakhirpamyourtis/api/RetrofitClient.kt
@@ -0,0 +1,27 @@
+package com.example.tugasakhirpamyourtis.api
+
+import kotlin.getValue
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = "http://10.0.2.2:3000/api/"
+
+    val instance: ApiService by lazy {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+            .create(ApiService::class.java)
+    }
+}
