bf00d7fe2cff4f00c3e5171fa00d7ddb7d490d88
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-13 10:19:52 +0700

    menambahkan dependensi Retrofit untuk networking, Gson untuk parsing JSON, dan Logging Interceptor untuk debugging API

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,4 +56,9 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    // 1. Retrofit & Gson (Wajib untuk API)
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // 2. Logging (Untuk mengintip data yang dikirim/diterima di Logcat)
+    implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
 }
