86f1addeee56dc5fab708771661175f06fb3643a
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-13 10:40:15 +0700

    komponen UI

---
+++ app/src/main/java/com/example/tugasakhirpamyourtis/LoginActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tugasakhirpamyourtis
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tugasakhirpamyourtis.api.RetrofitClient
+import com.example.tugasakhirpamyourtis.model.LoginResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        // 1. Kenalkan Komponen UI
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
+        val tvRegister = findViewById<TextView>(R.id.tvRegister)
+
+}
