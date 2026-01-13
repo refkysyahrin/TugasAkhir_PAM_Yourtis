cb73289f1c372660934b7814b2d05573a9690792
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2026-01-13 12:57:15 +0700

    wadah untuk menampung data yang dikirim dari Backend

---
+++ app/src/main/java/com/example/tugasakhirpamyourtis/model/Sayur.kt
@@ -0,0 +1,10 @@
+package com.example.tugasakhirpamyourtis.model
+
+data class Sayur(
+    val id_sayur: Int,
+    val nama_sayur: String,
+    val harga: Int,
+    val stok: Int,
+    val deskripsi: String,
+    val gambar: String
+)
